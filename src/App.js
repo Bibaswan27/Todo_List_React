6fd2ed2a62670906a8e07aff0ee5e01fe5d8e8a1
Bibaswan27 <116551393+Bibaswan27@users.noreply.github.com>
2024-08-19 16:17:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,37 +30,40 @@ function App() {
   };
 
   return (
-    <div style={{ backgroundColor: 'grey', display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-      <div>
-      <h2 style={{fontSize:50}}>Todo List</h2>
-      <div style={{flexDirection:'row', justifyContent: 'center', display: 'flex', lignItems: 'center'}}>
-        <input
-          type="text"
-          value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-          placeholder="Enter task"
-          style={{height:30,width:200}}
-        />
-        <FontAwesomeIcon icon={faPlus} style={{height:40}} onClick={handleAddTask} />
-      </div>
-      </div>
-      {tasks.length > 0 && (
-        <div >
-          <ul>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <div style={{ textAlign: 'center', padding: '20px' }}>
+        <h2 style={{ fontSize: '36px', marginBottom: '20px' }}>Todo List</h2>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <input
+            type="text"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            placeholder="Enter task"
+            style={{ padding: '10px', border: '1px solid #ccc', borderRadius: '5px', width: '200px' }}
+          />
+          <button style={{ marginLeft: '10px', padding: '10px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer' }} onClick={handleAddTask}>
+            <FontAwesomeIcon icon={faPlus} style={{ marginRight: '5px' }} />
+            Add Task
+          </button>
+        </div>
+        {tasks.length > 0 && (
+          <ul style={{ listStyle: 'none', padding: '0', marginTop: '20px' }}>
             {tasks.map((task, index) => (
-              <ul key={index}>
+              <li key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
                 <input
                   type="text"
                   value={task}
                   onChange={(e) => handleEditTask(index, e.target.value)}
+                  style={{ padding: '5px', border: '1px solid #ccc', borderRadius: '5px', width: '150px' }}
                 />
-                <FontAwesomeIcon icon={faTrashCan} onClick={() => handleDelTask(index)} />
-              </ul>
+                <FontAwesomeIcon icon={faTrashCan} style={{ marginLeft: '10px', color: 'red', cursor: 'pointer' }} onClick={() => handleDelTask(index)} />
+              </li>
             ))}
           </ul>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
+
 export default App;
